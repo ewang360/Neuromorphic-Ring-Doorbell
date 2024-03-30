7204689a33439940eeb5cc444ee04597cfd127e1
MarkCiora <mfc32@pitt.edu>
2024-03-29 21:56:39 -0400

    asdf

--- Mark/Hackathon.py
+++ Mark/Hackathon.py
@@ -7,37 +7,32 @@ def nothing(x):
 kernel = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
 camera = cv2.VideoCapture(0)
 spacial = 0
+PIX_ON_THRESH = 10
+PIX_OFF_THRESH = 10
 if camera.isOpened():
     verdict, frame1 = camera.read()
     cv2.namedWindow('SpacialImage')
-    cv2.createTrackbar('UpperThreshold', 'SpacialImage', 0, 50, nothing)
-    cv2.createTrackbar('LowerThreshold', 'SpacialImage', 0, 50, nothing)
-    cv2.createTrackbar('Spacial', 'SpacialImage', 0, 1, nothing)
     if verdict:
         while (True):
             _, frame2 = camera.read()
-            spacial = cv2.getTrackbarPos('Spacial', 'SpacialImage')
-            PIX_ON_THRESH = cv2.getTrackbarPos('UpperThreshold', 'SpacialImage')
-            PIX_OFF_THRESH = (-1*cv2.getTrackbarPos('LowerThreshold', 'SpacialImage'))
             #determine if this is spacial or temporal
-            if spacial:
-                #need to subtract images and determine difference in pixel values
-                diffFrame = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY).astype('int16')
-                newFrame = cv2.filter2D(diffFrame,-1,kernel)
-                #do the thresholding
-                newFrame[np.where(newFrame<(PIX_OFF_THRESH))] = -127
-                newFrame[np.where(newFrame >(PIX_ON_THRESH))] = 128
-                newFrame += 127
-                cv2.imshow('SpacialImage', newFrame.astype('uint8'))
+            diffFrame = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY).astype('int') - cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY).astype('int')
+            # do the thresholding
+            total = np.sum(diffFrame < PIX_OFF_THRESH) + np.sum(diffFrame > PIX_ON_THRESH)
+            diffFrame[np.where(diffFrame < (PIX_OFF_THRESH))] = -127
+            diffFrame[np.where(diffFrame > (PIX_ON_THRESH))] = 128
+            diffFrame += 127
+            cv2.imshow('SpacialImage', diffFrame.astype('uint8'))
+            # get new frame1
+            frame1 = frame2
+            if total > 100:
+                PIX_ON_THRESH = min(50,PIX_ON_THRESH+1)
+                PIX_OFF_THRESH = max(-50, PIX_OFF_THRESH-1)
             else:
-                diffFrame = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY).astype('int') - cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY).astype('int')
-                # do the thresholding
-                diffFrame[np.where(diffFrame < (PIX_OFF_THRESH))] = -127
-                diffFrame[np.where(diffFrame > (PIX_ON_THRESH))] = 128
-                diffFrame += 127
-                cv2.imshow('SpacialImage', diffFrame.astype('uint8'))
-                # get new frame1
-                frame1 = frame2
+                PIX_ON_THRESH = max(1,PIX_ON_THRESH-1)
+                PIX_OFF_THRESH = min(-1, PIX_OFF_THRESH+1)
+
+            print(PIX_ON_THRESH, PIX_OFF_THRESH, total)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 #make the dynamic ranges change with the number of events coming in
